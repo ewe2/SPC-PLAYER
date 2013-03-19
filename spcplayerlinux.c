cef6bd0e45783306b0751ee9a51251e98d0079ea
Sean Dwyer <ewetoo@gmail.com>
2013-03-20 07:58:55 +1100

    cleaned up audio interface sources

--- spcplayerlinux.c
+++ spcplayerlinux.c
@@ -19,95 +19,105 @@ Version 0.2
 
 int main(int argc, char *argv[])
 {
-int optionoffset=0,audio_fd,channels=2,rformat,rchannels,format=AFMT_S16_LE,speed=32000,fd;
-char audio_device[]="/dev/dsp";
-char ver[]="0.2";
-char c;
-void *ptr,*buf;
-off_t size;
+	int optionoffset=0,audio_fd,channels=2,rformat,rchannels,format=AFMT_S16_LE,speed=32000,fd;
+	char audio_device[]="/dev/dsp";
+	char ver[]="0.2";
+	char c;
+	void *ptr,*buf;
+	off_t size;
 
 
-if((argc<2))
-{
-printf("\n[?] Usage: soap [sound device] SPC_FILE_NAME\n[?] Optional parameters are in brackets.\n[?] SOAP Version %s (2003) Steve B Melvin Jr\n\n",ver);
-exit(1);
-}
+	if((argc<2))
+	{
+		printf("\n[?] Usage: soap [sound device] SPC_FILE_NAME\n[?] Optional parameters are in brackets.\n[?] SOAP Version %s (2003) Steve B Melvin Jr\n\n",ver);
+		exit(1);
+	}
 
-if((argc>2))
-{
-strcpy(audio_device,argv[1]);
-optionoffset++;
-}
+	if((argc>2))
+	{
+		strcpy(audio_device,argv[1]);
+		optionoffset++;
+	}
 
-if((audio_fd=open(audio_device, O_WRONLY, 0)) ==-1)
-{
- printf("[-] Could not open, %s.\n", audio_device);
- exit(1);
-}
-printf("[+] Successfully opened, %s.\n",audio_device);
+	if((audio_fd=open(audio_device, O_WRONLY, 0)) ==-1)
+	{
+ 		printf("[-] Could not open, %s.\n", audio_device);
+ 		exit(1);
+	}
+	
+	printf("[+] Successfully opened, %s.\n",audio_device);
 
-rformat=format;
-if(ioctl(audio_fd, SNDCTL_DSP_SETFMT, &format) == -1)
- {
- printf("[-] Could not set sound format.\n");
- exit(1);
- }
-if(format!=rformat)
-{
- printf("[-] Could not set sound format.\n");
- exit(1);
-}
-printf("[+] Successfully set sound format.\n");
+	rformat=format;
+	
+	if(ioctl(audio_fd, SNDCTL_DSP_SETFMT, &format) == -1)
+ 	{
+ 		printf("[-] Could not set sound format.\n");
+ 		exit(1);
+ 	}
 
-rchannels=channels;
-if(ioctl(audio_fd,SNDCTL_DSP_CHANNELS, &channels) == -1)
-{
-printf("[-] Could not set channels.\n");
-exit(1);
-}
-if(channels!=rchannels)
-{
-printf("[-] Could not set channels.\n");
-exit(1);
-}
-printf("[+] Successfully set channels.\n");
+	if(format!=rformat)
+	{
+ 		printf("[-] Could not set sound format.\n");
+ 		exit(1);
+	}
 
-if(ioctl(audio_fd, SNDCTL_DSP_SPEED, &speed)==-1)
-{
-printf("[-] Could not set speed.\n");
-exit(1);
-}
-printf("[+] Using speed, {%iHz}\n",speed);
+	printf("[+] Successfully set sound format.\n");
 
-buf=malloc(32000);
+	rchannels=channels;
 
-fd=open(argv[1+optionoffset],O_RDONLY);
-if(fd<0)
-{
-printf("[-] Could not open \'%s\'\n.",argv[1]);
-exit(1);
-}
+	if(ioctl(audio_fd,SNDCTL_DSP_CHANNELS, &channels) == -1)
+	{
+		printf("[-] Could not set channels.\n");
+		exit(1);
+	}
 
-size=lseek(fd,0,SEEK_END);
-lseek(fd,0,SEEK_SET);
-ptr=malloc(size);
-read(fd,ptr,size);
-close(fd);
+	if(channels!=rchannels)
+	{
+		printf("[-] Could not set channels.\n");
+		exit(1);
+	}
 
-fd=OSPC_Init(ptr,size);
+	printf("[+] Successfully set channels.\n");
 
-free(ptr);
-fcntl(STDIN_FILENO,F_SETFL,O_NONBLOCK);
-printf("[+] Playing SPC, press Enter to quit.\n");
+	if(ioctl(audio_fd, SNDCTL_DSP_SPEED, &speed)==-1)
+	{
+		printf("[-] Could not set speed.\n");
+		exit(1);
+	}
 
-while((read(STDIN_FILENO,&c,1))<=0)
-{
-size=OSPC_Run(-1,buf,32000);
-write(audio_fd,buf,size);
-}
+	printf("[+] Using speed, {%iHz}\n",speed);
 
-printf("\nGoodbye!\n");
-return 0;
+	buf=malloc(32000);
+
+	fd=open(argv[1+optionoffset],O_RDONLY);
+
+	if(fd<0)
+	{
+		printf("[-] Could not open \'%s\'\n.",argv[1]);
+		exit(1);
+	}
+
+	size=lseek(fd,0,SEEK_END);
+	lseek(fd,0,SEEK_SET);
+	ptr=malloc(size);
+	read(fd,ptr,size);
+	close(fd);
+
+	fd=OSPC_Init(ptr,size);
+
+	free(ptr);
+
+	fcntl(STDIN_FILENO,F_SETFL,O_NONBLOCK);
+	printf("[+] Playing SPC, press Enter to quit.\n");
+
+
+	while((read(STDIN_FILENO,&c,1))<=0) {
+		size=OSPC_Run(-1,buf,32000);
+	write(audio_fd,buf,size);
+	}
+
+	printf("\nGoodbye!\n");
+	return 0;
 }
 
 
